63f429dea64b190c043e61266c085f2eb2636e77
Emily Morgan <emorgan05@gmail.com>
2017-09-22 01:02:14 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(num)
+  if num == 1 || num == 2 || num == 3
+    true
+  else
+    sq = num ** 0.5
+    range = (2..sq).to_a
+    range.each do |divisor|
+      if num % divisor.to_f == 0.0
+        false
+      end
+    end
+  end
+end
+
+prime?(3)
